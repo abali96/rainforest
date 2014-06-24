0e76e791ff3e4b6033609d6bf9d736c6ddf3231b
Arjun <arjun.bali96@gmail.com>
2014-06-24 12:35:43 -0400

    creating log in functionality'

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    # we don't need to instantiate a user object
+    # because we are only logging in, not creating
+    # a new user
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password]) #if both of these are true
+      session[:user_id] = user.id #create a key-value pair in the sessions hash, with a session key :user_id
+      redirect_to products_url, :notice => "Logged in!"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_url, :notice => "Logged out!"
+  end
+end
